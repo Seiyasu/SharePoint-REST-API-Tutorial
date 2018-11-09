058ad908cb936f84f28d6256f18fece0318160fa
Chris Wang <Chrismiaow@gmail.com>
2018-11-09 14:53:25 +0800

    Adding addin smaple for rest api

---
+++ Addins/SharePoint Addin Rest API Tutorial/SharePoint Addin Rest API Tutorial/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="63a86b31-e92e-4336-b7e6-7d2ca57a0d39" description="SharePoint Add-in Feature" featureId="63a86b31-e92e-4336-b7e6-7d2ca57a0d39" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint Addin Rest API Tutorial Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="0c907886-4aba-4e85-8818-0c8910b1e2fd" />
+    <projectItemReference itemId="8bf3eac4-5ff4-4203-94cb-7fe4a067267f" />
+    <projectItemReference itemId="f1d8c6bb-6878-4d47-bc49-6aa7e6b53a34" />
+    <projectItemReference itemId="b2e2f992-d99f-4b3b-be8e-33a241c49553" />
+  </projectItems>
+</feature>
---
+++ Addins/SharePoint Addin Rest API Tutorial/SharePoint Addin Rest API Tutorial/Scripts/App.js
@@ -0,0 +1,51 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage() {
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+    $(document).ready(function () {
+        getUserName();
+    });
+
+    // This function prepares, loads, and then executes a SharePoint query to get the current users information
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+    }
+
+    // This function is executed if the above call is successful
+    // It replaces the contents of the 'message' element with the user name
+    function onGetUserNameSuccess() {
+        $('#message').text('Hello ' + user.get_title());
+    }
+
+    // This function is executed if the above call fails
+    function onGetUserNameFail(sender, args) {
+        alert('Failed to get user name. Error:' + args.get_message());
+    }
+
+    $(document).ready(function () {
+        $("#loadButton").click(usingLoad);
+    });
+
+    function usingLoad() {
+        var context = SP.ClientContext.get_current();
+        var web = context.get_web();
+        context.load(web);
+        context.executeQueryAsync(success, fail);
+
+        function success() {
+            var message = jQuery("#message");
+            message.text(web.get_title());
+            message.append("<br/>");
+            //message.append(lists.get_count());
+        }
+        function fail(sender, args) {
+            alert("Call failed. Error: " + args.get_message());
+        }
+    }
+}
